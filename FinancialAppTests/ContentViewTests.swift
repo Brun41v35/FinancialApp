24059a1ac9e24f1b9f488204992d7e3590c442e1
Bruno Alves <alves.bruno945@gmail.com>
2024-12-18 20:38:47 -0300

    Add ContentViewTests with test_zero

---
+++ FinancialAppTests/ContentViewTests.swift
@@ -0,0 +1,11 @@
+//  Created by Bruno Silva on 18/12/24.
+
+import XCTest
+@testable import FinancialApp
+
+final class ContentViewTests: XCTestCase {
+
+    func test_zero() {
+        XCTFail("Test zero")
+    }
+}
